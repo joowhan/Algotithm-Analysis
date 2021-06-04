9393089c78fb01b718b95850006616132a47af8a
joowhan <21700165@handong.edu>
2021-06-04 11:19:43 +0900

    fix bugs

--- hw6/hw6_21700165_JoowhanKim.cpp
+++ hw6/hw6_21700165_JoowhanKim.cpp
@@ -1,10 +1,19 @@
 /*
     **References**
     (1)lecture slide ch22.
-    
+
 */
 #include <iostream>
+#include <fstream>
+#include <vector>
+#include <string>
+#include <iterator>
+#include <sstream>
+#include <algorithm>
 
 int main(){
- int num;
+  #if 1
+  #else
+  int g;
+  #endif
 }
